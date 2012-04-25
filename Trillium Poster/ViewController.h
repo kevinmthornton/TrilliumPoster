77cddf45962f13cb70e82b7ef7d5a95da9b3b5d2
Kevin Thornton <kevinthornton@Kevins-MacBook-Pro.local>
2012-04-24 18:56:23 -0700

    Initial Commit

---
+++ Trillium Poster/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Trillium Poster
+//
+//  Created by Kevin Thornton on 4/24/12.
+//  Copyright (c) 2012 radisys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
